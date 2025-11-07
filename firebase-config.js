0f102062bb3d4fed0757949a45597138a4a5caf6
krishna chemudupati <krishkc@seas.upenn.edu>
2025-11-07 09:04:07 -0500

    Configure Firebase backend for real-time sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,18 @@
-// Firebase configuration for crossbar measurement tracker
-// This is a public Firebase project configured with open read/write rules for small team collaboration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ8X9vZ7xKj2mQYnF8L5wRpT4sU6vH3eM",
+  apiKey: "AIzaSyCnBJTK5KOuauyeS76nbJtn9kNKMe2ArMg",
   authDomain: "crossbar-tracker.firebaseapp.com",
   databaseURL: "https://crossbar-tracker-default-rtdb.firebaseio.com",
   projectId: "crossbar-tracker",
-  storageBucket: "crossbar-tracker.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abc123def456ghi789"
+  storageBucket: "crossbar-tracker.firebasestorage.app",
+  messagingSenderId: "959133316967",
+  appId: "1:959133316967:web:dffa64c39de004dd29a1c2"
 };
 
-// Note: These are placeholder credentials. You'll need to:
-// 1. Go to https://console.firebase.google.com/
-// 2. Create a new project named "crossbar-tracker"
-// 3. Enable Realtime Database
-// 4. Set database rules to allow public read/write (for small team use):
-//    {
-//      "rules": {
-//        ".read": true,
-//        ".write": true
-//      }
-//    }
-// 5. Replace the config above with your actual Firebase credentials from Project Settings > General
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
